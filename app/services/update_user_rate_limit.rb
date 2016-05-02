72254f8ddbaaa44d7ece35a17209cb1debf3b0db
Julien Letessier <julien.letessier@gmail.com>
2016-05-02 11:27:48 +0200

    Fixes rate limit updates

--- app/services/update_user_rate_limit.rb
+++ app/services/update_user_rate_limit.rb
@@ -5,9 +5,9 @@ class UpdateUserRateLimit
   end
 
   def run
-    @user.throttle_limit    = rate_limit.limit
-    @user.throttle_left     = rate_limit.remaining
-    @user.throttle_reset_at = rate_limit.resets_at
+    @user.throttle_limit    = @rate_limit.limit
+    @user.throttle_left     = @rate_limit.remaining
+    @user.throttle_reset_at = @rate_limit.resets_at
     @user.save! if @user.changed?
   end
 end
