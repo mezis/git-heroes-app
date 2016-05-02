b005eaaa2cc3b4aa33f8a7d42ca72986ef882c9e
Julien Letessier <julien.letessier@gmail.com>
2016-05-02 11:27:40 +0200

    Fixes required Github API permission scopes

--- config/initializers/omniauth_github.rb
+++ config/initializers/omniauth_github.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'],
     scope: %w[
       user:email
-      repo:status
+      repo
       read:org
       read:repo_hook
       write:repo_hook
