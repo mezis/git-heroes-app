8f01b80f12b39b92e41e6f7645a89b2ebd5061dd
Julien Letessier <julien.letessier@gmail.com>
2016-05-09 18:17:12 +0200

    Fixes scoring backfill edge cases

--- app/interactors/score_backfill.rb
+++ app/interactors/score_backfill.rb
@@ -4,7 +4,11 @@ class ScoreBackfill
   def call
     Organisation.find_each do |org|
       repository_ids = org.repositories.enabled.pluck(:id)
+      next unless repository_ids.any?
+
       oldest_pull_request = PullRequest.where(repository_id: repository_ids).order(:created_at).first
+      next unless oldest_pull_request.present?
+
       date = oldest_pull_request.created_at.beginning_of_week.to_date
       while date < Date.current
         ScoreUsersJob.perform_later organisation_id: org.id, date: date.to_s
