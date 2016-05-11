719eebc5aa00d20d1bba8ec2d451b1e6f03af1e0
Julien Letessier <julien.letessier@gmail.com>
2016-05-11 22:11:22 +0200

    Ignore events with missing type

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,10 +3,16 @@ class EventsController < ApplicationController
   before_filter :skip_authorization
 
   def create
-    IngestEventJob.perform_later(
-      event: request.headers['X-GitHub-Event'],
-      data:  JSON.parse(request.body.read),
-    )
+    event_type = request.headers['X-GitHub-Event']
+    if event_type.blank?
+      Rails.logger.warn 'Missing event type, ignoring'
+    else
+      IngestEventJob.perform_later(
+        event: event_type,
+        data:  JSON.parse(request.body.read),
+      )
+    end
+
     render nothing: true, status: 201
   end
 end
