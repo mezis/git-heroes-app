5f6b5ba1a0cba1fbf0cc01bbaab8bad0da4a22d7
Julien Letessier <julien.letessier@gmail.com>
2016-07-04 09:06:51 +0200

    Fixes team user updates

--- app/interactors/update_team_users.rb
+++ app/interactors/update_team_users.rb
@@ -4,24 +4,24 @@ class UpdateTeamUsers
   delegate :team, to: :context
 
   def call
+    # team users may appear in multiple Github role queries
     users = %w[maintainer member].each_with_object({}) do |role,h|
-      h[role] = paginate { 
+      paginate { 
         client.team_members(team.github_id, role: role)
-      }.map { |h|
-        FindOrCreateUser.call(data: h).record
+      }.map { |d|
+        user = FindOrCreateUser.call(data: d).record
+        h[user] ||= role
       }.to_a
     end
 
-    team_users = users.map do |role, users|
-      users.map do |u|
-        tu = team.team_users.find_or_initialize_by user: u
-        tu.role = role
-        tu
-      end
+    team_users = users.map do |u, role|
+      tu = team.team_users.find_or_initialize_by user: u
+      tu.role = role
+      tu
     end
 
     team.transaction do
-      team_users.flatten.each { |tu| tu.save! if tu.changed? }
+      team_users.select(&:changed?).each(&:save!)
       team.team_users = team_users.flatten
     end
   end
